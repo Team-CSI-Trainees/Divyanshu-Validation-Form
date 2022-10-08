b5dbd9d41314d94772638736266a67e3bccca152
divyansh453 <104566386+divyansh453@users.noreply.github.com>
2022-10-08 16:53:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
+let em=document.getElementById("suggestion");
+em.style.display="none";
+function hide() {
+    em.style.display="block";
+}
 function validateform() {
     let c=0;
     let w1=document.getElementById("warn1");
     let w2=document.getElementById("warn2");
     let f=document.getElementById("formdata");
+    let f1=document.getElementById("end");
     let x=document.forms["myform"]["name"].value;
+    let ph=document.forms["myform"]["phone"].value;
+    console.log(em);
     if( x==="")
-   {document.getElementById('warn1').innerHTML='<h3>fill name correctly </h3>';
+   {document.getElementById('warn1').innerHTML='<h3>Fill name Field correctly!!</h3>';
     }
    else
    {w1.style.display="none";
     c++;}
-   if((document.forms["myform"]["phone"].value.length)!=10)
-   {document.getElementById('warn2').innerHTML='<h3>fill phone number correctly </h3>';
-}
+   if(((ph.length)!=10)||(ph[0]==0))
+   {document.getElementById('warn2').innerHTML='<h3>Fill phone number correctly!!</h3>';}
    else
    {w2.style.display="none";
    c++;
     }
-    if(c==2)
+    if((/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/.test(myform.email.value)))
+    {
+        c++;
+    }
+    else
+    {document.getElementById("warn3").innerHTML="<h3>Fill email correctly</h3>";
+    }
+    if(c==3)
     {f.style.display="none";
-    document.getElementById("end").innerHTML="<h1>Your Form is Submitted</h1>";}
+    f1.style.display="flex";
+    document.getElementById("submit").style.display="none";
+    f1.innerHTML="<h1>Your Form is Submitted</h1>";}
     
 }
+
+
+
+
+
+
 
 
 
