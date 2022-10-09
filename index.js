743ba5f1cc19ef64c83a3b876f8a7b9ddca1e06d
divyansh453 <104566386+divyansh453@users.noreply.github.com>
2022-10-09 15:59:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
+var c=0;
 let em=document.getElementById("suggestion");
 em.style.display="none";
-function hide() {
+let w3=document.getElementById("warn4");
+function show() {
     em.style.display="block";
+    // w3.style.display="none";
+}
+function hide() {
+    em.style.display="none";
+    let pw=document.forms["myform"]["password"].value;
+    let pw1=document.forms["myform"]["password2"].value;
+    console.log(pw);
+    console.log(w3);
+    console.log(pw.length);
+    if((pw.length)<6)
+    {console.log("condition checked");
+    w3.style.display="block";
+    w3.innerHTML="<h3>Password length must be at least 6 Characters</h3>";
+    }
+   else
+   {w3.style.display="none";
+   return 1;
+    }   
 }
 function validateform() {
-    let c=0;
     let w1=document.getElementById("warn1");
     let w2=document.getElementById("warn2");
     let f=document.getElementById("formdata");
     let f1=document.getElementById("end");
     let x=document.forms["myform"]["name"].value;
     let ph=document.forms["myform"]["phone"].value;
+    console.log(ph);
     console.log(em);
     if( x==="")
    {document.getElementById('warn1').innerHTML='<h3>Fill name Field correctly!!</h3>';
@@ -29,21 +49,15 @@ function validateform() {
         c++;
     }
     else
-    {document.getElementById("warn3").innerHTML="<h3>Fill email correctly</h3>";
+    {document.getElementById("warn3").innerHTML="<h3>Fill email correctly!!</h3>";
     }
-    if(c==3)
+   c+=hide();
+   console.log(hide());
+   console.log(c);
+    if(c==4)
     {f.style.display="none";
     f1.style.display="flex";
     document.getElementById("submit").style.display="none";
     f1.innerHTML="<h1>Your Form is Submitted</h1>";}
+   c=0; 
     
-}
-
-
-
-
-
-
-
-
-
